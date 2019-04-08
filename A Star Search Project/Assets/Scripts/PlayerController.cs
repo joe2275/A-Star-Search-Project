d2cd381c7fa86aac5e475afcb116741aadcd6b62
hus5522 <hus5522@naver.com>
2019-04-08 21:14:25 +0900

    Update Player Script
    Player can move to position of result

--- A Star Search Project/Assets/Scripts/PlayerController.cs
+++ A Star Search Project/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     private Stack<Vector2Int> m_path;
 
     private bool m_isStarted = false;
+    private bool isArrived = true;
+
+    [SerializeField]
+    private Transform playerTransform;
+
+    private Vector2 nextPosition;
+    private Vector3 goalPosition = new Vector3(21,-19,0);
 
     // Update is called once per frame
     void Update()
     {
-        if(!m_isStarted)
+        if (!m_isStarted)
         {
             m_isStarted = true;
             PathFinder finder = new PathFinder();
             m_path = finder.FindPath();
-            while(m_path.Count != 0)
+        } else
+        {
+            if (playerTransform.localPosition != goalPosition)
             {
-                Debug.Log(m_path.Pop());
+                if (isArrived)
+                {
+                    nextPosition = WayPointManager.Instance.ConvertKeyToPosition(m_path.Pop());
+                    Debug.Log(nextPosition);
+                    isArrived = false;
+                }
+                else
+                {
+                    if (playerTransform.localPosition.x == nextPosition.x && playerTransform.localPosition.y == nextPosition.y)
+                    {
+                        isArrived = true;
+                    }
+                    playerTransform.position = Vector2.MoveTowards(playerTransform.localPosition, nextPosition, 20.0f * Time.deltaTime);
+                }
             }
         }
     }
